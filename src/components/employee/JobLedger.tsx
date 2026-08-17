9a92ccbc858c5ed43c7d7c675c3553ce8bfd94b5
hamid-osanoman-projects <hamid.osanoman@gmail.com>
2026-08-17 20:38:02 +0400

    fix: add missing isSalesMode closing divs causing OXC parse error

--- src/components/employee/JobLedger.tsx
+++ src/components/employee/JobLedger.tsx
@@ -1013,6 +1013,8 @@ export const JobLedger = ({ job, onPaymentReceived }: { job: any, onPaymentRecei
                 )}
               </div>
             </div>
+          </div>
+        )}
 
         {/* Total Summaries */}
 
